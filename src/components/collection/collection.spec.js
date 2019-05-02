3184d97c900e1a0dda52afef367d76e79225f123
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2019-05-02 14:17:46 -0400

    COMPASS-3524: Update view icon
    - Leave a failing test for including `view_on`
    - Clean up formatting

--- src/components/collection/collection.spec.js
+++ src/components/collection/collection.spec.js
@@ -8,13 +8,13 @@ import styles from './collection.less';
 
 class Documents extends React.Component {
   render() {
-    return (<div id="test">Testing</div>);
+    return <div id="test">Testing</div>;
   }
 }
 
 class CollectionStats extends React.Component {
   render() {
-    return (<div id="stats">Stats</div>);
+    return <div id="stats">Stats</div>;
   }
 }
 
@@ -24,14 +24,12 @@ const ROLE = {
 };
 
 const CollectionStore = Reflux.createStore({
-  setTabs() {
-
-  },
+  setTabs() {},
   getActiveTab() {
     return 0;
   },
   isReadonly() {
-    return false;
+    return true;
   }
 });
 
@@ -43,20 +41,50 @@ describe('Collection [Component]', () => {
   };
   let component;
 
+  // TODO (lucas): Update collection-store for views.
+  // const view = {
+  //   ns: 'echo.albums',
+  //   view_on: 'artist',
+  //   pipeline: [
+  //     { $unwind: '$albums' },
+  //     { $project: { artist: '$name', title: '$albums.name' } }
+  //   ]
+  // };
+
   beforeEach(() => {
-    global.hadronApp.appRegistry.registerComponent('CollectionStats.Component', CollectionStats);
-    global.hadronApp.appRegistry.registerStore('App.CollectionStore', CollectionStore);
+    global.hadronApp.appRegistry.registerComponent(
+      'CollectionStats.Component',
+      CollectionStats
+    );
+    global.hadronApp.appRegistry.registerStore(
+      'App.CollectionStore',
+      CollectionStore
+    );
     global.hadronApp.appRegistry.registerRole('Collection.Tab', ROLE);
     global.hadronApp.instance = instance;
-    component = mount(<Collection namespace="db.coll" />);
+    component = mount(<Collection namespace="echo.albums" />);
   });
 
   afterEach(() => {
     global.hadronApp.appRegistry.deregisterRole('Collection.Tab', ROLE);
+    global.hadronApp.appRegistry.deregisterStore('App.CollectionStore');
     component = null;
   });
 
   it('renders the correct root classname', () => {
     expect(component.find(`.${styles.collection}`)).to.be.present();
+  });
+
+  it('must show the view-icon', () => {
+    console.log(styles['collection-title-readonly-view-icon']);
+    expect(
+      component.find(`.${styles['collection-title-readonly-view-icon']}`)
+    ).to.be.present();
+  });
+
+  it('must include the collection name the view is based on', () => {
+    expect(
+      component.find(`.${styles['collection-title-readonly-view-on']}`).text()
+    ).to.include.text('artist');
   });
 });
